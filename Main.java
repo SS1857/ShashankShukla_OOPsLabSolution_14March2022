e70acc09db7cb13608b9a228fde55a1a1ad91922
SS1857 <89942518+SS1857@users.noreply.github.com>
2022-03-13 21:16:15 +0530

    Add files via upload
    EmailApplication Assignment

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.gl.emailapp;
+
+import com.gl.emailapp.ui.EmailApplicationUI;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		EmailApplicationUI ui = new EmailApplicationUI();
+		
+		ui.init();
+	}
+}
